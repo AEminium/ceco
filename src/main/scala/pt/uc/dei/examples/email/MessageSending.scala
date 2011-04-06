fe5d33453598bd5b5a207b985c35e220803fcb37
Alcides Fonseca <me@alcidesfonseca.com>
2011-04-06 15:38:45 -0400

    Improved example of MessageSending.

--- src/main/scala/pt/uc/dei/examples/email/MessageSending.scala
+++ src/main/scala/pt/uc/dei/examples/email/MessageSending.scala
@@ -5,8 +5,8 @@ import scala.actors.Actor
 import scala.actors.Actor._
 import pt.uc.dei.cehm._
 
-class AddressDoesNotExistException extends RemoteException
-class IOException extends RemoteException
+class AddressDoesNotExistException(val email:String) extends RemoteException
+class IOException(val email:String) extends RemoteException
 
 
 object MessageSending {
@@ -22,24 +22,38 @@ object messageController extends Actor with ExceptionModel {
     // initialize the system
     ExceptionController.start
     
-    var futures:List[MessageSender] = List()
-    _try {
-      (1 to 9).foreach { m =>
-        val ms = new MessageSender
-        ms.start
-        ms ! m
-        futures = ms :: futures
-      }
-      
-      futures.foreach{ m => m !? Stop }
-      _check
-      
-    } _catch {
-      e:RemoteException => 
-        e match {
-            case e:AddressDoesNotExistException => println("[Controller] Address does not exist")
-            case e:IOException => println("[Controller] IO error")
+    var senders:List[MessageSender] = List()
+    
+    var database:List[String] = (0 to 8).map{ n => ("employee" + n + "@company.com") }.toList
+    var recipients:List[String] = database
+    
+    while (!recipients.isEmpty) {
+      senders = List()
+      _try {
+        recipients.zipWithIndex.foreach { m =>
+          val ms = new MessageSender
+          ms.start
+          ms ! (m)
+          senders = ms :: senders
         }
+        recipients = List()
+    
+        senders.foreach{ m => m !? Stop }
+        _check
+    
+      } _catch {
+        e:RemoteException => 
+          e match {
+              case e:AddressDoesNotExistException => {
+                database = database - e.email
+                println("[Controller] " + e.email + " does not exist")
+              }
+              case e:IOException => {
+                recipients = e.email :: recipients
+                println("[Controller] IO error")
+              }
+          }
+      }
     }
     
     // Stop the system
@@ -47,21 +61,24 @@ object messageController extends Actor with ExceptionModel {
   }
 }
 
-class MessageSender extends Actor with ExceptionModel {   
-  def work(i:Integer) {
+class MessageSender extends Actor with ExceptionModel {
+  private var fakeError:Boolean = true
+    
+  def work(i:Integer, e:String) {
     _try {
       if (i == 3) {
         Thread.sleep(5*1000)
-        _throw (new AddressDoesNotExistException)
-      } else if ( i == 7) {
+        _throw (new AddressDoesNotExistException(e))
+      } else if ( i == 7 && fakeError) {
         Thread.sleep(5*1000)
-        _throw (new IOException)
+        fakeError = false
+        _throw (new IOException(e))
       }
       else {
-        println("[Sender " + i + "] Message " + i + " being sent.")
+        println("[Sender " + i + "] sending to " + e  + "...")
         Thread.sleep(10*1000)
         _check
-        println("[Sender " + i + "] Message " + i + " sent.")
+        println("[Sender " + i + "] sent.")
       }
     } _catch { e:Exception => 
       println("[Sender " + i + "] Got local exception: " + e)
@@ -75,10 +92,10 @@ class MessageSender extends Actor with ExceptionModel {
           sender ! Ack
           exit()
         }
-        case i:Int => {
-          work(i)
+        case i:Tuple2[String, Int] => {
+          work(i._2, i._1)
         }
-        case _ => ()
+        case i:Any => ()
       }
     }
   }
