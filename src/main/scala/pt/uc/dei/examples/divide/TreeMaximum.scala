071eb00bccabb401f5684c727011c838081b4dd9
Alcides Fonseca <me@alcidesfonseca.com>
2011-04-08 00:20:17 -0400

    Added comments to examples.

--- src/main/scala/pt/uc/dei/examples/divide/TreeMaximum.scala
+++ src/main/scala/pt/uc/dei/examples/divide/TreeMaximum.scala
@@ -1,3 +1,12 @@
+/*
+This example calculate the maximum value contained inside
+a binary tree with values on leaves.
+
+Since infinite values are allowed, unnecessary computation
+may be prevented using Concurrent exceptions.
+
+*/
+
 package pt.uc.dei.examples.divide
 
 import scala.collection.immutable.List
@@ -6,25 +15,43 @@ import scala.actors.Actor._
 import scala.util.Random
 import pt.uc.dei.cehm._
 
+
+/*
+Exception that should be raised shall an Infinite Value be found.
+In order to catch it from other actors,
+it must inherit from ConcurrentException
+*/
 class InfiniteValue extends ConcurrentException
 
+/* Recursive Tree data structure */
 sealed abstract class Tree
 case class Node(left: Tree, right: Tree) extends Tree
 case class EmptyNode[A](value:A) extends Tree
 
+/* Number class with support for Infinite value */
 sealed abstract class Number
 case class Real(n:Int) extends Number
 case object Inf extends Number
 
+/* Main method will only start the Controller method */
 object TreeMaximum {
   def main(args : Array[String]) {
       Controller.start
   }
 }
 
+/*
+The Controller actor requests the calculation.
+To use ConcurrentExceptions, the actor integrates the
+ExceptionModel trait.
+*/
 object Controller extends Actor with ExceptionModel {
   val rand = new Random
   
+  /*
+  Generates a new unbalanced tree with up to 10 levels,
+  filled with numbers from 1 to 8 and Infinite.
+  */
   def createRandomTree(n:Int):Tree = {
     if (rand.nextInt(10) < n) {
       def map(i:Int):Number = {
@@ -40,54 +67,81 @@ object Controller extends Actor with ExceptionModel {
   }
   
   def act() {
+    
+    /* ExceptionController delivers exceptions to interested actors */
     ExceptionController.start
     
     val tree = createRandomTree(0);
     val fjtask = new FJMaximum
     fjtask.start
+
+    /* Concurrent try */
     _try {
+      /* Synchronously request the maximum value. */
       val i:Any = fjtask !? tree
+      
+      /* Check wether any exceptions were raised. */
       _check
+      
       println("Maximum: " + i)
     } _catch {
+      /* Try block will only receive InfiniteValue exceptions. */
       e:InfiniteValue => println("Infinite value present in Tree")
     }
     
-    
+    /* Exception dispatching no longer required. */
     ExceptionController ! Stop
     exit()
   }
 }
 
+/*
+FJMaximum actor calculates the maximum value using
+a divide-and-conquer strategy. 
+
+It forks into two other actors of the same kind
+and awaits the answer from both and returns the maximum of both.
+
+Whenever an Infinite value is found, all calculation actors
+should abort their computations as they are no longer required.
+*/
 class FJMaximum extends Actor with ExceptionModel {   
   def act() {
     loop {
       react {
         case e:Tree => {
+          // Concurrent Try
           _try {
-            _check
+            _check // Check for exceptions before
             val ans = e match {
+              /* Handle the base case*/
               case n:EmptyNode[_] => n.value match {
-                  case Real(n) => n 
+                  case Real(n) => n // Just a number
                   case Inf => { 
-                    println("Found an infinit")
+                    println("Found an infinite value.")
+                    /* Stop execution and inform other actors */
                     _throw(new InfiniteValue)
-                    0
+                    0 // Required for type checking
                   }
               }
               case t:Node => {
+                /* Recursive fork */
                 def process(tr:Tree):Future[Any] = {
                   val fjtask = new FJMaximum
                   fjtask.start
                   fjtask !! tr
                 }
+                /* Re-check for Exceptions  */
                 _check
+                
+                /* Merge the two results*/
                 val r:Int = process(t.right)().asInstanceOf[Int]
                 val l:Int = process(t.left)().asInstanceOf[Int]
                 if (r > l) r else l
               }
             }
             _check
+            /* Required to unblock parent call. */
             sender ! ans
           } _catch {
               e:InfiniteValue => {
--- src/main/scala/pt/uc/dei/examples/email/MessageSending.scala
+++ src/main/scala/pt/uc/dei/examples/email/MessageSending.scala
@@ -1,3 +1,15 @@
+/*
+This example represents a batch sending of email messages.
+For each email a new actor is created, benefitting of the parallelism,
+and two exceptions may be raised, and caught by the MessageController:
+
+* Invalid Address: if such address does not exist, it should be removed
+from database.
+
+* IOException: Something went wrong with the communication channels and
+the MessageController should retry to send it.
+
+*/
 package pt.uc.dei.examples.email
 
 import scala.collection.immutable.List
@@ -5,50 +17,75 @@ import scala.actors.Actor
 import scala.actors.Actor._
 import pt.uc.dei.cehm._
 
+
+/* Both exceptions are ConcurrentExceptions  */
 class InvalidAddressException(val email:String) extends ConcurrentException
 class IOException(val email:String) extends ConcurrentException
 
 
 object MessageSending {
   def main(args : Array[String]) {
-      messageController.start
+      MessageController.start
   }
 }
 
-object messageController extends Actor with ExceptionModel {   
+/*
+MessageController is responsible for sending a message to each email
+address on a database.
+*/
+object MessageController extends Actor with ExceptionModel {   
   def act() {
     println("[Controller] Starting to send messages..")
     
-    // initialize the system
+    // ExceptionController delivers exceptions to interested actors
     ExceptionController.start
     
+    var database:List[String] = (0 to 8).map{ n => ("employee" + n + "@company.com") }.toList
+    
+    // Will store current sender actors
     var senders:List[MessageSender] = List()
     
-    var database:List[String] = (0 to 8).map{ n => ("employee" + n + "@company.com") }.toList
+    // Send to all addresses in database.
     var recipients:List[String] = database
     
     while (!recipients.isEmpty) {
       senders = List()
+      
+      // Concurrent try
       _try {
         recipients.zipWithIndex.foreach { m =>
+          /* spawns a new MessageSender actor for each email */
           val ms = new MessageSender
           ms.start
           ms ! (m)
           senders = ms :: senders
         }
+        /* Resets the recipient lists */
         recipients = List()
-    
+        
+        /* Awaits for all senders to finish */
         senders.foreach{ m => m !? Stop }
+        
+        /*
+          Verifies wether any exceptions were thrown by senders
+          In this case it is important to have explicitly, in order
+          to ask for all addresses first.
+        */
         _check
     
       } _catch {
+        /*
+          We are interested in ConcurrentExceptions. 
+        */
         e:ConcurrentException => 
           e match {
               case e:InvalidAddressException => {
+                /* If the address does not exist, remove the email from database  */
                 database = database - e.email
-                println("[Controller] " + e.email + " does not exist")
+                println("[Controller] " + e.email + " removed from database")
               }
               case e:IOException => {
+                /* In case of IO error, retry */
                 recipients = e.email :: recipients
                 println("[Controller] IO error")
               }
@@ -56,35 +93,53 @@ object messageController extends Actor with ExceptionModel {
       }
     }
     
-    // Stop the system
+    // No longer needed.
     ExceptionController ! Stop
   }
 }
 
+
+/* 
+  MessageSender will send an email to a given address.
+  It may raise InvalidAddressException or IOException.
+  
+  Behavior is faked. Each actor will await 10 minutes before returning.
+  Email #3 will raise an InvalidAddressException.
+  Email #7 will raise an IOException the first time it is sent.
+*/
 class MessageSender extends Actor with ExceptionModel {
   private var fakeError:Boolean = true
     
   def work(i:Integer, e:String) {
+    /* Local, regular try. See _catch for details */
     _try {
       if (i == 3) {
         Thread.sleep(5*1000)
+        
+        // Raise the exception for the controller and terminate execution
         _throw (new InvalidAddressException(e))
       } else if ( i == 7 && fakeError) {
         Thread.sleep(5*1000)
         fakeError = false
+        // Same here, but with a different kind of exception
         _throw (new IOException(e))
       }
       else {
+        // Regular behavior.
         println("[Sender " + i + "] sending to " + e  + "...")
         Thread.sleep(10*1000)
-        _check
         println("[Sender " + i + "] sent.")
       }
     } _catch { e:Exception => 
+      /*
+      When catching for regular Exception, all the regular behavior
+      of a traditional try-catch is kept.
+      */
       println("[Sender " + i + "] Got local exception: " + e)
     }
   }
   
+  /* Actor message handling details. */
   def act() {
     loop {
       react {
