8ddfd129707afb64a26035c3df77697057829a34
Alcides Fonseca <me@alcidesfonseca.com>
2011-12-17 13:50:53 +0000

    PlainTreeMaximum working.

--- src/main/scala/examples/divide/PlainTreeMaximum.scala
+++ src/main/scala/examples/divide/PlainTreeMaximum.scala
@@ -6,72 +6,78 @@ import scala.actors.Actor._
 import scala.util.Random
 
 object PlainTreeMaximum {
-  def main(args : Array[String]) {
-      PlainController.start
-  }
+	def main(args : Array[String]) {
+		PlainController.start
+	}
 }
 
+object ErrorCounter {
+	var hasErrors = false
+}
 
 object PlainController extends Actor {
-  
-  def act() {
-    val tree = TreeFactory.createRandomTree(0);
-    val fjtask = new PlainFJMaximum
-    fjtask.start
-    
-    val r:Any = fjtask !? tree
-    
-    r match {
-      case i:Int => println("Maximum: " + i)
-      case _ => println("Infinite value present in Tree")
-    }
-    exit()
-  }
+
+	def act() {
+		val tree = TreeFactory.createRandomTree(0);
+		val fjtask = new PlainFJMaximum
+		fjtask.start
+
+		val r:Any = fjtask !? tree
+		if (ErrorCounter.hasErrors) {
+			println("Infinite value present in Tree")
+		} else {
+			println("Maximum: " + r.asInstanceOf[Int])
+		}
+		exit()
+	}
 }
 
 class PlainFJMaximum extends Actor {   
-  def act() {
-    loop {
-      react {
-        case e:Tree => {
-          val ans = e match {
-             /* Handle the base case*/
-              case n:EmptyNode[_] => n.value match {
-                  case Real(n) => n // Just a number
-                  case Inf => new InfiniteValue
-              }
-              case t:Node => {
-                /* Recursive fork */
-                def process(tr:Tree):Future[Any] = {
-                  val fjtask = new PlainFJMaximum
-                  fjtask.start
-                  fjtask !! tr
-                }
-                
-                /* Merge the two results*/
-                val r:Any = process(t.right)()
-                val l:Any = process(t.left)()
-                r match {
-                  case e:Int => {
-                    l match {
-                      case e:Int => {
-                        val ri = r.asInstanceOf[Int]
-                        val li = l.asInstanceOf[Int]
-                        if (ri > li) ri else li
-                      } 
-                      case _ => l
-                    }
-                  }
-                  case _ => r
-                }
-                
-              }
-          }          
-          /* Required to unblock parent call. */
-          sender ! ans
-          exit()
-        }
-      }
-    }
-  }
+	def act() {
+		loop {
+			react {
+				case e:Tree => {
+					val ans = e match {
+						/* Handle the base case*/
+						case n:EmptyNode[_] => n.value match {
+							case Real(n) => n // Just a number
+							case Inf => {
+								ErrorCounter.hasErrors = true
+								new InfiniteValue
+							}
+						}
+						case t:Node => {
+							try {
+								/* Recursive fork */
+								def process(tr:Tree):Future[Any] = {
+									val fjtask = new PlainFJMaximum
+									fjtask.start
+									fjtask !! tr
+								}
+								if (ErrorCounter.hasErrors) throw new InfiniteValue
+								
+								/* Merge the two results*/
+								val r:Any = process(t.right)()
+								val l:Any = process(t.left)()
+								
+								if (ErrorCounter.hasErrors) throw new InfiniteValue
+
+								val ri = r.asInstanceOf[Int]
+								val li = l.asInstanceOf[Int]
+								if (ri > li) ri else li
+							} catch {
+								case e:InfiniteValue => {
+									println("Aborting Computation")
+									0
+								}
+							}
+						}
+					}          
+					/* Required to unblock parent call. */
+					sender ! ans
+					exit()
+				}
+			}
+		}
+	}
 }
